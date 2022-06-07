c196d153862ca8e3ac31c2bb82e6a6a997d0b252
mostafa akhlak <106383879+mostafaakhlak@users.noreply.github.com>
2022-06-07 13:31:19 +0200

    Add files via upload

---
+++ Reg.h
@@ -0,0 +1,15 @@
+/*
+ * Reg.h
+ *
+ *  Created on: 07/06/2022
+ *      Author: Mostafa Akhlak Mohamed
+ */
+
+#ifndef REG_H_
+#define REG_H_
+
+#define DDRC	*(volatile char*)0x34
+#define PORTC	*(volatile char*)0x35
+
+
+#endif /* REG_H_ */
